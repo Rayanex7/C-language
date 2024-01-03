b2fd00f9f8722778e689dcdb307000db964be6c7
Rayanex7 <lyachaarayan@gmail.com>
2024-01-04 00:50:23 +0100

    Calculate the earning of the day

---
+++ 3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+float nbsales, price;
+	printf("Enter the amount of sales : ");
+	scanf("%f", &nbsales);
+	printf("Enter the price : ");
+	scanf("%f", &price);
+	float sum;
+	sum = nbsales * price;
+	printf("The amount of sales is %.0f\n", sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
